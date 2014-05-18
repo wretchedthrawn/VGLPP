e12147ec343846d4584ad2224d2aa0bbdcc44b7c
Mike Farrell <mf321907@gmail.com>
2014-05-18 12:59:14 -0700

    Initial Commit

---
+++ VGLPP Demo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VGLPP Demo
+//
+//  Created by Mike Farrell on 5/18/14.
+//  Copyright (c) 2014 Mike Farrell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
